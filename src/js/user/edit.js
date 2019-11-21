01fb68f7eedb255542163defd71eeda9d61d3d7f
rdvhoorn <robinvh@live.nl>
2019-11-21 11:11:18 +0100

    Checkboxes show which groups user is part of

--- src/js/user/edit.js
+++ src/js/user/edit.js
@@ -1,15 +1,35 @@
 var app = angular.module("confluente");
 
 app.controller("userEditController", ["$scope", "$q", "$routeParams", "users", "groups", function ($scope, $q, $routeParams, user, groups) {
+    var userId = $routeParams.userId;
     $q.all([
         groups.getAll().then(function (groups) {
             $scope.groups = groups;
+        }),
+        user.get(userId).then(function (param) {
+            $scope.user = param[0];
+            $scope.member = param[1];
         })
     ]).then(function () {
-        var userId = $routeParams.userId;
-        user.get(userId).then(function (user) {
-            $scope.user = user;
-        });
+        let member_groups = [];
+        let i;
+        for (i = 0; i < $scope.member.length; i++) {
+            member_groups.push($scope.member[i].id);
+        }
+
+        $scope.groupSelection = [];
+        for (i = 0; i < $scope.groups.length; i++) {
+            if (member_groups.includes($scope.groups[i].id)) {
+                $scope.groupSelection.push({fullName: $scope.groups[i].fullName, selected: true})
+            } else {
+                $scope.groupSelection.push({fullName: $scope.groups[i].fullName, selected: false})
+            }
+        }
+
+        // Helper method
+        $scope.selectedGroups = function selectedGroups() {
+            return filterFilter($scope.groupSelection, {selected: true});
+        };
 
         $scope.loading = false;
         $scope.submit = function () {
