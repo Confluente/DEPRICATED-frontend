15fc1a1870128d15af8acbda27e4695fbcc16cf4
HiddeKoerts <hiddekoerts@live.nl>
2020-01-08 21:25:14 +0100

    Page titles in browser tabs fixed for pages including later-loading snippets

--- src/js/page/view.js
+++ src/js/page/view.js
@@ -1,13 +1,16 @@
 var app = angular.module("confluente");
-app.controller("pageViewController", ["$rootScope", "$scope", "$routeParams", function ($rootScope, $scope, $routeParams) {
+app.controller("pageViewController", ["$rootScope", "$scope", "$routeParams", "$location", function ($rootScope, $scope, $routeParams, $location) {
     $scope.loading = true;
     $scope.templateUrl = getTemplateUrl($routeParams);
 
     var fallbackUrl = "/www/pages/404.html";
 
+    /**
+     * Sets the page title (used in browser-tab label)
+     */
     $scope.$on("$includeContentLoaded", function (e, src) {
         //Boo yah!
-        $rootScope.title = getPageTitle(src);
+        $rootScope.title = getPageTitle($location);
     });
 
     $scope.$on("$includeContentError", function (e, src) {
@@ -26,8 +29,11 @@ function getTemplateUrl(routeParams) {
     return "/www/pages/" + routeParams.url + ".html";
 }
 
-function getPageTitle(url) {
-    var name = url.split("/").slice(-1)[0].split(".")[0];
+/**
+ * Gets the current URL and deduced the page title from it
+ */
+function getPageTitle($location) {
+    var name = $location.path().split("/").slice(-1)[0].split(".")[0];
     var words = name.split('_');
     var title = '';
     for (var i = 0; i < words.length; i++) {
