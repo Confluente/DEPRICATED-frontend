9ada762dce35442fad85042e1ca415303deb5de3
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2020-01-20 17:16:33 +0100

    Implemented portrait right consent notifcation

---
+++ src/js/notifications/portraitRight.js
@@ -0,0 +1,23 @@
+var app = angular.module("confluente");
+
+/**
+ * Controller for consent to portrait right
+ */
+app.controller("portraitRightController", ["$rootScope", "$scope", "notifications", function ($rootScope, $scope, notifications) {
+    $scope.loading = false;
+    $scope.consent = function () {
+        $scope.loading = true;
+        notifications.portraitRight($rootScope.user.id, true).then(function (result) {
+            $scope.loading = false;
+            window.location.href = "/user/profile";
+        })
+    }
+}]);
+
+module.exports = {
+    name: "Portrait right consent",
+    url: "/consent_portrait_right",
+    parent: "/",
+    templateUrl: "/www/pages/consentPortraitRight.html",
+    controller: "portraitRightController"
+};
