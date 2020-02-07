f3a84cbe3f24208e0bb660fa69ca69008e90aad4
rdvhoorn <robinvh@live.nl>
2020-02-07 16:57:38 +0100

    Scroll carousel to second image on loading

--- src/js/home/view.js
+++ src/js/home/view.js
@@ -1,8 +1,15 @@
+var app = angular.module("confluente");
+
+app.controller("homePageController", ["$scope", "$routeParams", function ($scope, $routeParams) {
+    document.getElementById("rightCarouselButton").click();
+}]);
+
 module.exports = {
     id: 1,
     name: "Home",
     url: "/",
     parent: null,
     templateUrl: "/www/home.html",
-    iconUrl: "/img/home-outline.png"
+    iconUrl: "/img/home-outline.png",
+    controller: "homePageController"
 };
