477a8b4a7df2575956768a895eca6f4493035fae
Martijn Leus <martijnleus@live.nl>
2020-03-16 19:17:18 +0100

    Change how images are shown, and add links.

--- src/js/committees/index.js
+++ src/js/committees/index.js
@@ -8,7 +8,7 @@ app.controller("committeesController", ["$scope", "groups", function ($scope, gr
     groups.getAllOfType("committee").then(function(committees) {
 
         committees.forEach(function(comm) {
-            // comm.link = "/groups/" + comm.id.toString();
+            comm.link = "/groups/" + comm.id.toString();
             comm.image = "/img/CommitteePictures/" + comm.displayName.replace(/ /g, "_").toLowerCase() + ".jpg";
         });
 
