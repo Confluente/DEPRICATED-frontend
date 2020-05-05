57217ecad61e7f4f17f5746298648674ed86b041
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2020-05-05 11:51:35 +0200

    Uploading and retrieving images works now for all formats jpeg, jpg, png, gif

--- src/js/activities/view.js
+++ src/js/activities/view.js
@@ -63,12 +63,6 @@ app.controller("activityViewController", ["$scope", "$routeParams", "activities"
         // check if the current date is passed the subscription deadline
         var now = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
         if (now > $scope.activity.subscriptionDeadline) $scope.deadlinePassed = true;
-
-        if ($scope.activity.hasCoverImage) {
-            $scope.activity.imageSource = "img/activities/" + $scope.activity.id + ".jpeg"
-        }
-
-        console.log($scope);
     });
 
     // redirects user to login page
