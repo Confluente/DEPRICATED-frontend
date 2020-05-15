20adc896cafa7ebb63b02367a1c0515292565453
HiddeKoerts <hiddekoerts@live.nl>
2020-05-15 11:09:46 +0200

    Allow immediate publishing upon activity creation

--- src/js/activities/create.js
+++ src/js/activities/create.js
@@ -73,7 +73,7 @@ app.controller("activityCreateController", ["$scope", "activities", function ($s
             endTime: $scope.endTime,
             location: $scope.location,
             participationFee: $scope.participationFee,
-            published: true,
+            published: $scope.published,
             canSubscribe: $scope.canSubscribe,
         };
 
