9a73e2fceb9d1b1445a17854286ea7be5c3cfbc2
Samuel <s.a.j.oosterholt@student.tue.nl>
2020-06-08 14:35:47 +0200

    Added comments

--- src/js/committees/index.js
+++ src/js/committees/index.js
@@ -1,17 +1,21 @@
 var app = angular.module("confluente");
 
+/* Load all committees from the database and pass them to the template. */
 app.controller("committeesController", ["$scope", "groups", function ($scope, groups) {
 
     $scope.loading = true;
 
-    /* Load all committees from the database and pass them to the template. */
+    /* Grab all the Committees from the database */
     groups.getAllOfType("Committee").then(function(committees) {
 
+        /* Bind home page and the image to the committee template */
         committees.forEach(function(committee) {
             committee.link = "/group/" + committee.id.toString();
+            /* Image is under src/img/CommitteePictures/Committee_Name.jpg */
             committee.image = "/img/CommitteePictures/" + committee.displayName.replace(/ /g, "_").toLowerCase() + ".jpg";
         });
 
+        /* Bind committees to scope */
         $scope.committees = committees;
         $scope.loading = false;
     });
